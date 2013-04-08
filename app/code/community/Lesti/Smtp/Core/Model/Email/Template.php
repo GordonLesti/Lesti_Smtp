b9d9c0683c46168a6e2b0aca79169378c3bfccee
Gordon <g.lesti@k-newmedia.de>
2013-04-08 12:22:46 +0200

    remove duplicate content

--- app/code/community/Lesti/Smtp/Core/Model/Email/Template.php
+++ app/code/community/Lesti/Smtp/Core/Model/Email/Template.php
@@ -9,14 +9,6 @@
 class Lesti_Smtp_Core_Model_Email_Template extends Mage_Core_Model_Email_Template
 {
 
-    const XML_PATH_LESTI_SMTP_ENABLE = 'system/lesti_smtp/enable';
-    const XML_PATH_LESTI_SMTP_HOST = 'system/lesti_smtp/host';
-    const XML_PATH_LESTI_SMTP_PORT = 'system/lesti_smtp/port';
-    const XML_PATH_LESTI_SMTP_USERNAME = 'system/lesti_smtp/username';
-    const XML_PATH_LESTI_SMTP_PASSWORD = 'system/lesti_smtp/password';
-    const XML_PATH_LESTI_SMTP_SSL = 'system/lesti_smtp/ssl';
-    const XML_PATH_LESTI_SMTP_AUTH = 'system/lesti_smtp/auth';
-
     /**
      * Send mail to recipient
      *
@@ -62,17 +54,8 @@ class Lesti_Smtp_Core_Model_Email_Template extends Mage_Core_Model_Email_Templat
                 break;
         }
 
-        if(Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_ENABLE)) {
-            $authDetails = array('port' => Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_PORT),
-                'auth' => Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_AUTH),
-                'username' => Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_USERNAME),
-                'password' => Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_PASSWORD));
-            if(Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_SSL) != Lesti_Smtp_Model_System_Config_Source_Ssl::NO) {
-                $authDetails['ssl'] = Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_SSL);
-            }
-            $transport = new Zend_Mail_Transport_Smtp(Mage::getStoreConfig(self::XML_PATH_LESTI_SMTP_HOST),
-                $authDetails);
-            Zend_Mail::setDefaultTransport($transport);
+        if(Mage::getStoreConfig(Lesti_Smtp_Helper_Data::XML_PATH_LESTI_SMTP_ENABLE)) {
+            Mage::helper('smtp')->setSmtpAsDefaultTransport();
         } else {
             if ($returnPathEmail !== null) {
                 $mailTransport = new Zend_Mail_Transport_Sendmail("-f".$returnPathEmail);
--- app/code/community/Lesti/Smtp/Model/Observer.php
+++ app/code/community/Lesti/Smtp/Model/Observer.php
@@ -11,17 +11,8 @@ class Lesti_Smtp_Model_Observer
 
     public function controllerFrontInitBefore($observer)
     {
-        if(Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_ENABLE)) {
-            $authDetails = array('port' => Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_PORT),
-                'auth' => Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_AUTH),
-                'username' => Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_USERNAME),
-                'password' => Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_PASSWORD));
-            if(Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_SSL) != Lesti_Smtp_Model_System_Config_Source_Ssl::NO) {
-                $authDetails['ssl'] = Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_SSL);
-            }
-            $transport = new Zend_Mail_Transport_Smtp(Mage::getStoreConfig(Lesti_Smtp_Core_Model_Email_Template::XML_PATH_LESTI_SMTP_HOST),
-                $authDetails);
-            Zend_Mail::setDefaultTransport($transport);
+        if(Mage::getStoreConfig(Lesti_Smtp_Helper_Data::XML_PATH_LESTI_SMTP_ENABLE)) {
+            Mage::helper('smtp')->setSmtpAsDefaultTransport();
         }
     }
 
